ce87bc4c0cfa280a06ce16fcf5ab40a9ce66ceb5
wei zhang <wzhangpku@gmail.com>
2019-02-21 21:33:56 +0100

    update plot script for ex2

--- plot-scripts/ex2_plot_bin_from_traj.py
+++ plot-scripts/ex2_plot_bin_from_traj.py
@@ -48,7 +48,7 @@ normal_z = sqrt(2 * pi * stiff_eps) * erf( pi/(2 * sqrt(2*stiff_eps)) )
 normal_z = int_norm()
 print 'normalization constant Z=%.4e\n' % normal_z
 theta_density = [ exp(-x*x*0.5/stiff_eps) / normal_z * cos(x) for x in angle_vec] 
-ax1.plot(angle_vec, theta_density, color='k',linewidth=0.1, linestyle=':', label=r'exact') 
+ax1.plot(angle_vec, theta_density, color='k',linewidth=1.0, linestyle=':', label=r'exact') 
 ax1.set_xlim(-pi * 0.5, pi * 0.5)
 ax1.set_ylim(-0.5, 6.0)
 ax1.set_xticks([-pi/2, -pi/4, 0, pi/4, pi/2]) 
@@ -76,8 +76,8 @@ counter_data_1 = [ (float(x) * 1.0 / (n * bin_width)) for x in data_file.readlin
 angle_vec = np.linspace( bin_width * 0.5, 2*pi-bin_width*0.5, n_bin, endpoint=True )
 
 ax2.plot(angle_vec, counter_data_0, color='r',linewidth=1.5, linestyle='-.', label=r'$h=0.0002$') 
-ax2.plot(angle_vec, counter_data_1, color='k',linewidth=1.5, linestyle='-', label=r'$h=0.005$') 
-ax2.axhline(1.0/(2*pi), 0, 2*pi, color='k', linewidth=0.1, linestyle=':', label=r'exact')
+ax2.plot(angle_vec, counter_data_1, color='k',linewidth=1.0, linestyle='-', label=r'$h=0.005$') 
+ax2.axhline(1.0/(2*pi), 0, 2*pi, color='k', linewidth=1.0, linestyle=':', label=r'exact')
 ax2.set_xlim(0, 2 * pi)
 ax2.set_ylim(-0.05, 0.22)
 ax2.set_xticks([0, pi/2, pi, pi*1.5, 2*pi]) 
@@ -110,7 +110,7 @@ normal_z = sqrt(2 * pi * stiff_eps) * erf( pi/(2 * sqrt(2*stiff_eps)) )
 normal_z = int_norm()
 print 'normalization constant Z=%.4e\n' % normal_z
 theta_density = [ exp(-x*x*0.5/stiff_eps) / normal_z * cos(x) for x in angle_vec] 
-ax1.plot(angle_vec, theta_density, color='k',linewidth=0.1, linestyle=':', label=r'exact') 
+ax1.plot(angle_vec, theta_density, color='k',linewidth=1.0, linestyle=':', label=r'exact') 
 ax1.set_xlim(-pi * 0.5, pi * 0.5)
 ax1.set_ylim(-0.5, 6.0)
 ax1.set_xticks([-pi/2, -pi/4, 0, pi/4, pi/2]) 
@@ -131,7 +131,7 @@ counter_data_0 = [ (float(x) * 1.0 / (n * bin_width)) for x in data_file.readlin
 angle_vec = np.linspace( bin_width * 0.5, 2*pi-bin_width*0.5, n_bin, endpoint=True )
 
 ax2.plot(angle_vec, counter_data_0, color='r',linewidth=1.5, linestyle='-.', label=r'$h=0.01$') 
-ax2.axhline(1.0/(2*pi), 0, 2*pi, color='k', linewidth=0.1, linestyle=':', label=r'exact')
+ax2.axhline(1.0/(2*pi), 0, 2*pi, color='k', linewidth=1.0, linestyle=':', label=r'exact')
 ax2.set_xlim(0, 2 * pi)
 ax2.set_ylim(0.05, 0.22)
 ax2.set_xticks([0, pi/2, pi, pi*1.5, 2*pi]) 
